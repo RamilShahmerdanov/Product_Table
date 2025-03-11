3ad11d377c995c6fa9429a1675e4c7c04f9d8894
RamilShahmerdanov <shahmerdanovr20@gmail.com>
2025-03-11 21:26:33 +0400

    Refactor Productcarts component, add loading state, and fix import issue

--- src/App.jsx
+++ src/App.jsx
@@ -65,6 +65,19 @@ function App() {
     );
   }
 
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setLoading(true); // Data yüklənməyə başlayır
+    setTimeout(() => {
+      setLoading(false); // 1 saniyədən sonra loading false olur
+    }, 1000);
+    localStorage.setItem("addedproduct", JSON.stringify(selectedProducts));
+  }, [selectedProducts]);
+
+  if (loading) {
+    return <h2 className={styles.loading}>Loading...</h2>; // ⬅️ Ekranda Loading göstər
+  }
+
   return (
     <>
       <div className={styles.mainbody}>
@@ -75,7 +88,6 @@ function App() {
           increaseCount={increaseCount} // Sayı artıran funksiyanı ötür
           decreaseCount={decreaseCount} // Sayı azaldan funksiyanı ötür
         />
-      
 
         <Productcarts
           addToTable={addToTable}
