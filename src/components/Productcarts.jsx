70113c323294d0016f2b246f821b735b05d50695
RamilShahmerdanov <shahmerdanovr20@gmail.com>
2025-03-11 21:06:03 +0400

    Refactor Productcarts component and fix import issue

--- src/components/Productcarts.jsx
+++ src/components/Productcarts.jsx
@@ -2,9 +2,8 @@ import styles from "./product.module.css";
 import React, { useEffect, useState } from "react";
 
 import { Getproducts } from "../services/Api";
-// import Cart from "./cart";
+import Cart from "./cart";
 import Cart from "./cart.jsx";
-
 
 function Productcarts({ addToTable, selectedProducts }) {
   const [products, setProducts] = useState([]);
